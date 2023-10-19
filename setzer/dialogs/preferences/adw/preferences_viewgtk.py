31c822f8f88fefa610f2b63c263741c1ae1d9680
kinten108101 <kinten108101@protonmail.com>
2023-10-19 17:09:43 +0700

    preferences: Switch page with alt+num

--- setzer/dialogs/preferences/adw/preferences_viewgtk.py
+++ setzer/dialogs/preferences/adw/preferences_viewgtk.py
@@ -1,6 +1,6 @@
 import gi
 gi.require_version('Gtk', '4.0')
-from gi.repository import Adw
+from gi.repository import Gtk, Adw
 
 
 class NoteBook(object):
@@ -17,7 +17,27 @@ class NoteBook(object):
             print(e)
 
 
+PreferencesWindowXml = '''\
+<interface>
+    <template class="SetzerPreferencesAdwWindow" parent="AdwPreferencesWindow">
+        <child>
+            <object class="GtkShortcutController" id="shortcut_controller">
+            </object>
+        </child>
+    </template>
+</interface>
+'''
+
+
+@Gtk.Template(string=PreferencesWindowXml)
 class PreferencesWindow(Adw.PreferencesWindow):
+    __gtype_name__ = "SetzerPreferencesAdwWindow"
+
+    shortcut_controller = Gtk.Template.Child()
+
+    def __init__(self):
+        super().__init__()
+        self.shortcut_controller.set_scope(Gtk.ShortcutScope.LOCAL)
 
     def connect(self, signal, callback, *args):
         if (signal == 'response'):
